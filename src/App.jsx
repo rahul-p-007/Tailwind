a8fba3d87166b5e5ad68ddc06d6ab4f6844d17dc
CODExR <rahulp00rtx@gmail.com>
2025-04-05 11:57:07 +0530

    some pratice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <GridElement />
+    </div>
+  );
 }
 
 export default App;
+function FlexElement() {
+  return (
+    <div className="bg-blue-300 flex gap-2  justify-around">
+      <div className="bg-red-400">Child 1</div>
+      <div className="bg-green-300">Child 2</div>
+      <div className="bg-violet-300">Child 4</div>
+    </div>
+  );
+}
+function GridElement() {
+  return (
+    <div
+      className="sm:grid sm:rounded-md grid-cols-12 inline  m-5
+    "
+    >
+      <div className="bg-red-400 col-span-5 text-2xl">Child 1</div>
+      <div className="bg-green-300 col-span-5">Child 2</div>
+      <div className="bg-violet-300 col-span-2">Child 4</div>
+    </div>
+  );
+}
